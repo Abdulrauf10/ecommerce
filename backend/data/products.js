f93879c8b90bcf18cba554c2ed49b99368957d2c
Muhammad Rauf <abd.rauf.lamada@gmail.com>
2021-09-24 22:16:58 +0700

    transferring database into mongoDB atalas

---
+++ backend/data/products.js
@@ -0,0 +1,46 @@
+const products = [
+  {
+    name: "iPhone 12",
+    imageUrl: "https://unsplash.com/photos/TpXoTb1uR5A",
+    description: "iPhone 12 Pro Silver, Pacific Blue, Gold, & Graphite",
+    price: 1000,
+    countInStock: 150
+  },
+  {
+    name: "varmillo",
+    imageUrl: "https://www.google.com/search?q=programmer+keyboard&sxsrf=AOaemvLVJHKh5PFV0F40xKSwE9DTLRfWHA:1632491116124&source=lnms&tbm=isch&sa=X&ved=2ahUKEwip7syo35fzAhV2IbcAHcMZASsQ_AUoAXoECAEQAw&biw=1920&bih=969&dpr=1#imgrc=azXNXYQ6PdcehM",
+    description: "Best recommended keyboard for programmer",
+    price: 700,
+    countInStock: 100
+  },
+  {
+    name: "Canon",
+    imageUrl: "https://www.google.com/search?q=product+for+canon+&tbm=isch&ved=2ahUKEwjfvviR4JfzAhU9KqYKHcDFCl0Q2-cCegQIABAA&oq=product+for+canon+&gs_lcp=CgNpbWcQAzIHCCMQ7wMQJzoGCAAQBRAeOgQIABAYOgUIABCABDoGCAAQCBAeUJvPAVjwgAJgh5UCaAZwAHgAgAGQAYgBrQmSAQMwLjmYAQCgAQGqAQtnd3Mtd2l6LWltZ8ABAQ&sclient=img&ei=SddNYZ_2Ab3UmAXAi6voBQ&bih=969&biw=1920#imgrc=SsAuvEBEraWInM",
+    description: "Capture the future with theeos system",
+    price: 800,
+    countInStock: 200 
+  },
+  {
+    name: "Canon",
+    imageUrl: "https://www.google.com/search?q=air+macbook+product&tbm=isch&ved=2ahUKEwj-0siW4ZfzAhV1sksFHeaNCucQ2-cCegQIABAA&oq=air+macbook+product&gs_lcp=CgNpbWcQAzIGCAAQCBAeOgUIABCABDoECAAQHjoGCAAQBRAeUNnYA1iV7gNgqPIDaABwAHgAgAGUAYgB8gWSAQM3LjGYAQCgAQGqAQtnd3Mtd2l6LWltZ8ABAQ&sclient=img&ei=X9hNYb6nCvXkrtoP5puquA4&bih=969&biw=1920#imgrc=gpiSO2gzhkT_1M",
+    description: "Apple MacBook Air 13-inch with M1 chip, 7-core GPU, 256GB SSD (Gold) [2020]",
+    price: 1100,
+    countInStock: 250 
+  },
+  {
+    name: "Atomic Habits",
+    imageUrl: "https://www.google.com/search?q=atomic+habit+book&tbm=isch&ved=2ahUKEwi_gfm14ZfzAhVSXisKHeyjAkAQ2-cCegQIABAA&oq=atomic+habit+&gs_lcp=CgNpbWcQARgBMgUIABCABDIFCAAQgAQyBQgAEIAEMgUIABCABDIFCAAQgAQyBQgAEIAEMgUIABCABDIFCAAQgAQyBQgAEIAEMgUIABCABDoKCCMQ7wMQ6gIQJzoHCCMQ7wMQJzoECAAQQzoICAAQgAQQsQM6BwgAELEDEEM6CwgAEIAEELEDEIMBUPXOFVidhBZg5-cWaAFwAHgAgAGHAYgBywmSAQQxMC4zmAEAoAEBqgELZ3dzLXdpei1pbWewAQrAAQE&sclient=img&ei=oNhNYb-xO9K8rQHsx4qABA&bih=969&biw=1920#imgrc=k5zEkuC_ybh_CM",
+    description: "Atomic Habits: An Easy & Proven Way to Build Good Habits & Break Bad Ones",
+    price: 12,
+    countInStock: 1000 
+  },
+  {
+    name: "Samsung",
+    imageUrl: "https://www.google.com/search?q=new+samsung+phone+2021&tbm=isch&ved=2ahUKEwicx67p4pfzAhUGoUsFHfwFCbIQ2-cCegQIABAA&oq=new+samsung&gs_lcp=CgNpbWcQARgCMgUIABCABDIFCAAQgAQyBQgAEIAEMgUIABCABDIFCAAQgAQyBQgAEIAEMgUIABCABDIFCAAQgAQyBQgAEIAEMgUIABCABDoKCCMQ7wMQ6gIQJzoHCCMQ7wMQJzoICAAQgAQQsQM6BQgAELEDOgQIABBDOgcIABCxAxBDOggIABCxAxCDAToLCAAQgAQQsQMQgwFQsK0PWNGLEmCirxJoAXAAeASAAfUDiAHZMpIBBjQtMTQuMZgBAKABAaoBC2d3cy13aXotaW1nsAEKwAEB&sclient=img&ei=GdpNYdzWDobCrtoP_IukkAs&bih=969&biw=1920#imgrc=TtqSyAoi6jM9AM",
+    description: "Samsung Galaxy S21 ",
+    price: 1500,
+    countInStock: 300 
+  }
+]
+
+module.exports = products;
--- backend/server.js
+++ backend/server.js
@@ -2,10 +2,15 @@ require("dotenv").config();
 const express = require ("express");
 const {PORT= 8000} = process.env;
 const connectDB = require ("./config/db.js");
+const productRoutes = require ("./routes/productRoutes");
 
 connectDB();
 
 const app = express();
+
+app.use(express.json());
+
+app.use("/api/products", productRoutes)
 
 app.listen(PORT, () => {
   console.log(`servers is running on port ${PORT}`)
